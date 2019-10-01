cce786ae506ec2bef78ec5887b1c8ae8b8208828
Dankrad Feist <mail@dankradfeist.de>
2019-10-01 21:44:17 +0100

    Fix check_challenges test

--- tests/test_check_challenges.py
+++ tests/test_check_challenges.py
@@ -27,23 +27,22 @@ def jacobi_bit_multi(a, n, m):
 @pytest.mark.parametrize(
     'challenge_no,challenge', challenges.items()
 )
-def test_check_challenge(legendre_bit_contract,
-                      w3,
-                      challenge_no,
-                      challenge):
-    print(challenge_no, challenge)
-    call = legendre_bit_contract.functions.challenges__redeemed(challenge_no)
+def test_check_challenge(legendre_bounty_contract,
+                         w3,
+                         challenge_no,
+                         challenge):
+    call = legendre_bounty_contract.functions.challenges__redeemed(challenge_no)
     redeemed = call.call()
     assert not redeemed
-    call = legendre_bit_contract.functions.challenges__bounty(challenge_no)
+    call = legendre_bounty_contract.functions.challenges__bounty(challenge_no)
     bounty = call.call()
     assert bounty == challenge["bounty"]
-    call = legendre_bit_contract.functions.challenges__check_length(challenge_no)
+    call = legendre_bounty_contract.functions.challenges__check_length(challenge_no)
     check_length = call.call()
     assert check_length == challenge["check_length"]    
-    call = legendre_bit_contract.functions.challenges__check_value(challenge_no)
+    call = legendre_bounty_contract.functions.challenges__check_value(challenge_no)
     check_value = call.call()
     assert check_value == challenge["check_value"]
-    call = legendre_bit_contract.functions.challenges__prime(challenge_no)
+    call = legendre_bounty_contract.functions.challenges__prime(challenge_no)
     prime = call.call()
     assert prime == challenge["prime"]
